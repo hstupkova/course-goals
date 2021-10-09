4827f05d6d8fd818755d0a058a80827eb2aa5e01
Hana Stupková <hstupkova@gmail.com>
2021-10-09 19:24:16 +0200

    CSS modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,5 +1,9 @@
-import styled from "styled-components";
+// import styled from "styled-components";
+import React from "react";
 
+import styles from './Button.module.css';
+
+/*
 const Button = styled.button`
   font: inherit;
   padding: 0.5rem 1.5rem;
@@ -25,15 +29,14 @@ const Button = styled.button`
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `;
+*/
 
-/*
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
 };
-*/
 
 export default Button;
